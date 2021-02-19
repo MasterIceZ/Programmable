e0e2b539c73848821629f4effaf52e2e3d14ba11
Borworntat Dendumrongkul <mastericez890@gmail.com>
2021-02-19 09:46:43 +0700

    Init

---
+++ sudoku_solver/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int table[10][10];
+
+void get_table();
+	int n,m;
+	n = m = 3;
+	vector<vector<int>>t(n,vector<int>(m));
+	for(int i=1;i<=n;++i){
+		for(int j=1;j<=n;++i){
+			cin >> tablt[i][j];
+		}
+	}
+}
+
+int32_t main (void){
+	ios::sync_with_stdio(0); cin.tie(0);
+	get_table();
+	return 0;
+}
