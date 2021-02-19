0381b25877a80e21efd9acbf6bdddd6531436a70
Borworntat Dendumrongkul <mastericez890@gmail.com>
2021-02-19 10:01:09 +0700

    Wrong Table -> Horizontal, Vertical

--- sudoku_solver/main.cpp
+++ sudoku_solver/main.cpp
@@ -4,17 +4,38 @@ using namespace std;
 
 int table[10][10];
 
-bool check_table(){
 
+bool check_table_2(){
+	
+	return true;
 }
 
-void get_table();
-	int n,m;
-	n = m = 3;
-	vector<vector<int>>t(n,vector<int>(m));
+//Use for Vertical and Horizontal line only
+bool check_table(){
+	//It looks like O(n^2) but it is O(1)
+	//Because of using Const in the game!
+	for(int i=1;i<=9;++i){
+		for(int j=1;j<=9;++j){
+			for(int k=1;k+i<=9;++k){
+				if(table[i][j] == table[i+k][j]){
+					return false;
+				}
+			}	
+			for(int k=1;k+j<=9;++k){
+				if(table[i][j] == table[i][j+k]){
+					return false;
+				}
+			}	
+		}
+	}	
+	return true;
+}
+
+void get_table(){
+	int n = 3;
 	for(int i=1;i<=n;++i){
-		for(int j=1;j<=n;++i){
-			cin >> tablt[i][j];
+		for(int j=1;j<=n;++j){
+			cin >> table[i][j];
 		}
 	}
 }
@@ -23,8 +44,12 @@ int32_t main (void){
 	ios::sync_with_stdio(0); cin.tie(0);
 	get_table();
 	if(!check_table()){
-		cout << "Wrong Table" ;
+		cout << "Wrong Table" << endl;
 		return 0;	
-	}		
+	}	
+	if(!check_table_2()){
+		cout << "Wrong Table" << endl; 
+		return 0;
+	}	
 	return 0;
 }
